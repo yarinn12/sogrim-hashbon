ce8a05bf92f3377b42a393af24bdca83d1a37928
yarinn12 <yarinn12@gmail.com>
2026-09-01 18:10:25 +0300

    Prepare store build 4.21 (149)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const PWA_RELEASE = "433";
-const CACHE_NAME = "settle-friends-live-v433";
+const PWA_RELEASE = "434";
+const CACHE_NAME = "settle-friends-live-v434";
 const CACHE_FILES = [
   "/",
   "/index.html",
