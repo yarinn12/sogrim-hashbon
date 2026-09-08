9bc79e3895dd04fe3554cf8eaf062f1aac30148f
yarinn12 <yarinn12@gmail.com>
2026-09-08 12:51:26 +0300

    Release 4.39 with verified account-link and group-joining fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const PWA_RELEASE = "487";
-const CACHE_NAME = "settle-friends-live-v487";
+const PWA_RELEASE = "488";
+const CACHE_NAME = "settle-friends-live-v488";
 const CACHE_PREFIX = "settle-friends-live-v";
 const NETWORK_FIRST_TIMEOUT_MS = 6_000;
 const CACHE_FILES = [
